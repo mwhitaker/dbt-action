ef358642c12947f9fd389461c14ae45ebe5d69f9
Michael Whitaker <monitus@gmail.com>
2020-10-06 10:07:20 -0700

    remove comments

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,17 +20,14 @@ fi
 DBT_LOG_FILE=${DBT_LOG_FILE:="dbt_console_output.txt"}
 DBT_LOG_PATH="${INPUT_DBT_PROJECT_FOLDER}/${DBT_LOG_FILE}"
 echo "DBT_LOG_PATH=${DBT_LOG_PATH}" >> $GITHUB_ENV
-# echo "::set-env name=DBT_LOG_PATH::${DBT_LOG_PATH}"
 echo "saving console output in \"${DBT_LOG_PATH}\""
 $1 2>&1 | tee "${DBT_LOG_FILE}"
 if [ $? -eq 0 ]
   then
-    # echo "::set-env name=DBT_RUN_STATE::passed"
     echo "DBT_RUN_STATE=passed" >> $GITHUB_ENV
     echo "::set-output name=result::passed"
     echo "DBT run OK" >> "${DBT_LOG_FILE}"
   else
-    # echo "::set-env name=DBT_RUN_STATE::failed"
     echo "DBT_RUN_STATE=failed" >> $GITHUB_ENV
     echo "::set-output name=result::failed"
     echo "DBT run failed" >> "${DBT_LOG_FILE}"
