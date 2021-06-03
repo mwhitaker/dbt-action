42191196fa50a01915329f9ec3f9cf1cd0a42396
Michael Whitaker <monitus@gmail.com>
2021-06-03 11:09:44 -0700

    better conditionals for supplied tokens

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,9 @@ cd ${INPUT_DBT_PROJECT_FOLDER}
 if [ -n "${DBT_BIGQUERY_TOKEN}" ]
 then
   echo trying to parse bigquery token
-  if $(echo ${DBT_BIGQUERY_TOKEN} | base64 -d > ./creds.json 2>/dev/null)
+  $(echo ${DBT_BIGQUERY_TOKEN} | base64 -d > ./creds.json 2>/dev/null)
+  success=$?
+  if [ $success -eq 0 ]
   then
     echo success parsing base64 encoded token
   elif $(echo ${DBT_BIGQUERY_TOKEN} > ./creds.json)
@@ -18,19 +20,17 @@ then
     echo cannot parse token
     exit 1
   fi
-fi
-
-if [ -n "${DBT_USER}" ] && [ -n "$DBT_PASSWORD" ]
+elif [ -n "${DBT_USER}" ] && [ -n "$DBT_PASSWORD" ]
 then
  echo trying to use user/password
- sed -i "s/_user_/${DBT_USER}/g" ./profiles.yml
- sed -i "s/_password_/${DBT_PASSWORD}/g" ./profiles.yml
-fi
-
-if [ -n "${DBT_TOKEN}" ]
+ sed -i "s/_user_/${DBT_USER}/g" ./datab.yml
+ sed -i "s/_password_/${DBT_PASSWORD}/g" ./datab.yml
+elif [ -n "${DBT_TOKEN}" ]
 then
  echo trying to use DBT_TOKEN for databricks
- sed -i "s/_token_/${DBT_TOKEN}/g" ./profiles.yml
+ sed -i "s/_token_/${DBT_TOKEN}/g" ./datab.yml
+else
+  echo no tokens supplied
 fi
 
 DBT_LOG_FILE=${DBT_LOG_FILE:="dbt_console_output.txt"}
